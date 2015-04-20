0d71314639ffe2f6512f8cbca58b48d96d433a21
Jenny <jenncui@gmail.com>
2015-04-20 03:15:16 -0400

    so far...

--- tumblr_mailer.js
+++ tumblr_mailer.js
@@ -1,7 +1,12 @@
 var fs = require('fs');
+var ejs = require('ejs');
+var tumblr = require('tumblr.js');
 
 var csvFile = fs.readFileSync("friend_list.csv","utf8");
 var emailTemplate = fs.readFileSync('email_template.html', 'utf8');
+
+var latestPosts = [];
+
 
 function csvParse(csvFile){
     var arrayOfObjects = [];
@@ -25,24 +30,40 @@ function csvParse(csvFile){
     return arrayOfObjects;
 }
 
-friendList = csvParse(csvFile);
+var friendList = csvParse(csvFile);
+console.log(friendList);
 
 friendList.forEach(function(row){
-
     var firstName = row["firstName"];
     var numMonthsSinceContact = row["numMonthsSinceContact"];
-
-    // we make a copy of the emailTemplate variable to a new variable to ensure
-       // we don't edit the original template text since we'll need to us it for 
-       // multiple emails
+    var customizedTemplate = ejs.render(emailTemplate, {
+        firstName: firstName,
+        numMonthsSinceContact: numMonthsSinceContact,
+        latestPosts: latestPosts
+    });
 
     var templateCopy = emailTemplate;
 
     // use .replace to replace FIRST_NAME and NUM_MONTHS_SINCE_CONTACT with firstName and  monthsSinceLastContact  
-    templateCopy = templateCopy.replace(/FIRST_NAME/gi,
-    firstName).replace(/NUM_MONTHS_SINCE_CONTACT/gi, numMonthsSinceContact);
+    templateCopy = templateCopy.replace(/firstName/gi,
+    firstName).replace(/numMonthsSinceContact/gi, numMonthsSinceContact);
 
-    console.log(templateCopy);
+var client = tumblr.createClient({
+  consumer_key: 'xxxxx',
+  consumer_secret: 'xxxxx',
+  token: 'xxxxx',
+  token_secret: 'xxxxx'
+});
 
 
+});
+client.posts('codingmeow.tumblr.com', function(err, blog){
+  var currentTime = new Date().getTime();
+  var blogTime = new Date(posts.date).getTime();
+  var timeDiff = Math.abs(currentTime - blogTime)/24/60/60/1000;
+  blog.posts.forEach(function(post){
+    if (timeDiff <= 7) {
+        latestPosts.push(post);
+    }
+    });
 });
